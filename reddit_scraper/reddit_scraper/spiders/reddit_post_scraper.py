4406430952fac938497582103cb33911abf8b4c6
KevHg <kevhung17@gmail.com>
2019-09-23 13:00:34 +0800

    Non-working domain argument

--- reddit_scraper/reddit_scraper/spiders/reddit_post_scraper.py
+++ reddit_scraper/reddit_scraper/spiders/reddit_post_scraper.py
@@ -6,24 +6,18 @@ from ..items import RedditPost
 
 class RedditPostScraper(CrawlSpider):
     name = 'reddit-posts'
-    allowed_domains = ['old.reddit.com']
 
-    start_urls = ['https://old.reddit.com/r/MouseReview/']
-    # start_urls = ['https://www.google.com/search?as_sitesearch=reddit.com/r/MouseReview&q=g305']
+    def __init__(self, domain='', *args, **kwargs):
+        super(CrawlSpider, self).__init__(*args, **kwargs)
+        self.allowed_domains = ['old.reddit.com']
+        self.start_urls = [domain]
+        # start_urls = ['https://www.google.com/search?as_sitesearch=reddit.com/r/MouseReview&q=g305']
 
     rules = (
         Rule(LinkExtractor(allow=('/comments/',)), callback='parse_item'),
     )
 
     def parse_item(self, response):
-        # self.logger.info(f'Crawling into {response.url}')
-        # item = RedditPost()
-        # item.title = response.css('h1::text').get()
-        # item.comments = response.css('div[data-test-id=comment] div p::text').getall()
-        # item.upvotes = response.xpath("//*[contains(text(), 'points')]").getall()
-        #
-        # return item
-
         yield {
             'title': response.css('a[data-event-action=title]::text').get(),
             # 'comments': response.css('div[class=md] p::text').getall(),
